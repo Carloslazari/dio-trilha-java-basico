f1ab2aa955f0ff60dabfebe791b496c7c8d116b3
Carloslazari <carlos.lazarie@gmail.com>
2023-09-11 13:54:51 -0300

    adicionando controle de fluxo

---
+++ DeafioControleFluxo/src/Contador/Contador.java
@@ -0,0 +1,37 @@
+package Contador;
+
+import java.util.Scanner;
+import Contador.ParametrosInvalidosException;
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException exception) {
+			exception.printStackTrace();
+		}	
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		
+		int contagem = parametroDois - parametroUm;
+		//realizar o for para imprimir os números com base na variável contagem
+		if(contagem <0) {
+			throw new ParametrosInvalidosException();
+		}
+		else {
+			for(int contador=1;contador<=contagem;contador++) {
+				System.out.println("Imprimindo numero "+contador);
+			}
+		}
+	}
+}
---
+++ DeafioControleFluxo/src/Contador/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+package Contador;
+
+public class ParametrosInvalidosException extends Exception {
+
+	public ParametrosInvalidosException() {
+		super("O segundo parâmetro deve ser maior que o primeiro");
+		// TODO Auto-generated constructor stub
+	}
+	 
+}
